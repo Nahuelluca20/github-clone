9fef08c37a2c6103f82db3a1080c33e039dbd330
Nahuel Luca <nahuelluca20@gmail.com>
2023-09-17 13:25:51 -0300

    finish navbat prototype

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,12 @@
+import Avatar from "assets/avatar.jpg";
 import Image from "next/image";
 import PlusSVG from "assets/plus.svg";
 import SearchSVG from "assets/search.svg";
 import PromptSvg from "assets/prompt.svg";
+import DropDownSvg from "assets/drop-down.svg";
+import InboxSvg from "assets/inbox.svg";
+import GithubSvg from "assets/github-logo.svg";
+import MenuSvg from "assets/menu.svg";
 
 import LayoutComponents from "./commons/LayoutComponents";
 import Tooltip from "./Tooltip";
@@ -9,26 +14,65 @@ import Tooltip from "./Tooltip";
 export default function Navbar() {
   return (
     <header>
-      <nav className="p-4 bg-githubDark-200 w-full flex items-center">
-        <span>Dashboard</span>
-        <LayoutComponents>
-          <Image alt={"search"} height={16} src={SearchSVG} width={16} />
-          <span className="text-xs flex items-center gap-2 w-52 text-githubGray-100">
-            Type
-            <kbd className="border rounded py-[0.5px] px-1 border-githubGray-200 text-xs">/</kbd>
-            to search
-          </span>
-          <div className="h-[19px] w-[1px] mx-2 bg-githubGray-200" />
-          <Tooltip text="Command palette">
-            <Image alt={"search"} height={20} src={PromptSvg} width={20} />
-          </Tooltip>
-        </LayoutComponents>
-        <div className="h-[19px] w-[1px] mx-2 bg-githubGray-200" />
-        <LayoutComponents>
-          <Tooltip text="Create new...">
-            <Image alt={"plus"} height={16} src={PlusSVG} width={16} />
-          </Tooltip>
-        </LayoutComponents>
+      <nav className="px-3 py-3 shadow-inner border-b border-githubGray-150 bg-githubDark-200 w-full flex items-center justify-between">
+        <ul className="flex gap-3 items-center">
+          <LayoutComponents>
+            <Image alt={"github"} height={20} src={MenuSvg} width={20} />
+          </LayoutComponents>
+          <Image
+            alt={"github"}
+            className="rounded-full p-1"
+            height={42}
+            src={GithubSvg}
+            width={42}
+          />
+          <span className="text-sm text-githubGray-50 font-bold">Dashboard</span>
+        </ul>
+        <ul className="flex items-center gap-4">
+          <li>
+            <LayoutComponents>
+              <Image alt={"search"} height={16} src={SearchSVG} width={16} />
+              <span className="text-xs flex items-center gap-2 w-52 text-githubGray-100">
+                Type
+                <kbd className="border rounded py-[0.5px] px-1 border-githubGray-200 text-xs">
+                  /
+                </kbd>
+                to search
+              </span>
+              <div className="h-[19px] w-[1px] mx-2 bg-githubGray-200" />
+              <Tooltip text="Command palette">
+                <Image alt={"search"} height={20} src={PromptSvg} width={20} />
+              </Tooltip>
+            </LayoutComponents>
+          </li>
+          <li>
+            <div className="h-[19px] w-[1px]  bg-githubGray-200" />
+          </li>
+          <li>
+            <Tooltip text="Create new...">
+              <LayoutComponents className="gap-1">
+                <Image alt={"plus"} height={16} src={PlusSVG} width={16} />
+                <Image alt={"DropDownSvg"} height={16} src={DropDownSvg} width={16} />
+              </LayoutComponents>
+            </Tooltip>
+          </li>
+          <li>
+            <Tooltip text="You have no unread notifications">
+              <LayoutComponents className="gap-1">
+                <Image alt={"Inbox"} height={18} src={InboxSvg} width={18} />
+              </LayoutComponents>
+            </Tooltip>
+          </li>
+          <li>
+            <Image
+              alt="Rounded avatar"
+              className="rounded-full"
+              height={32}
+              src={Avatar}
+              width={32}
+            />
+          </li>
+        </ul>
       </nav>
     </header>
   );
--- app/components/commons/LayoutComponents.tsx
+++ app/components/commons/LayoutComponents.tsx
@@ -1,7 +1,14 @@
-export default function LayoutComponents({children}: {children: React.ReactNode}) {
-  return (
-    <div className="cursor-pointer flex items-center h-full p-1.5 border-[1.8px] border-githubGray-200 rounded-md	w-fit">
-      {children}
-    </div>
-  );
+export default function LayoutComponents({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  const baseClasses =
+    "cursor-pointer flex items-center h-full p-1.5 border-[1.8px] border-githubGray-200 rounded-md w-fit";
+
+  const containerClasses = className ? `${baseClasses} ${className}` : baseClasses;
+
+  return <div className={containerClasses}>{children}</div>;
 }
